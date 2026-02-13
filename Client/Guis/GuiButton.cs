cca57cb1a577c063555e0043aa713d81370763f9
Fazin85 <grahamderson1@gmail.com>
2026-02-12 23:15:28 -0500

    Fix slider text color issue

--- Client/Guis/GuiButton.cs
+++ Client/Guis/GuiButton.cs
@@ -8,7 +8,7 @@ namespace betareborn.Client.Guis
         protected const int HOVER_STATE_DISABLED = 0;
         protected const int HOVER_STATE_NORMAL = 1;
         protected const int HOVER_STATE_HOVERED = 2;
-        
+
         protected int width;
         protected int height;
         public int xPosition;
@@ -64,20 +64,18 @@ namespace betareborn.Client.Guis
                 drawTexturedModalRect(xPosition, yPosition, 0, 46 + hoverState * 20, width / 2, height);
                 drawTexturedModalRect(xPosition + width / 2, yPosition, 200 - width / 2, 46 + hoverState * 20, width / 2, height);
                 mouseDragged(mc, mouseX, mouseY);
-                int textColor;
-                switch (hoverState)
+                if (!enabled)
                 {
-                    case HOVER_STATE_DISABLED:
-                        textColor = -6250336;
-                        break;
-                    case HOVER_STATE_HOVERED:
-                        textColor = 16777120;
-                        break;
-                    default:
-                        textColor = 14737632;
-                        break;
+                    drawCenteredString(font, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, -6250336);
                 }
-                drawCenteredString(font, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, textColor);
+                else if (isHovered)
+                {
+                    drawCenteredString(font, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, 16777120);
+                }
+                else
+                {
+                    drawCenteredString(font, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, 14737632);
+                }
 
             }
         }
