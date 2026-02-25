d957f94afa0f44e9d72d076fdd49cb1ae8d8d6a4
vos6434 <kacper11122003@wp.pl>
2026-02-25 02:32:52 +0000

    fix mod version info

--- HungerMod/ModInfoAssembly.cs
+++ HungerMod/ModInfoAssembly.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
 using BetaSharp.Modding;
 
 [assembly: ModInfoAttribute("Hunger Mod", "Valheim-style timed food buffs for health and regeneration.", "vos6434")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
--- SeeAllItems/ModInfoAssembly.cs
+++ SeeAllItems/ModInfoAssembly.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
 using BetaSharp.Modding;
 
-[assembly: ModInfoAttribute("See All Items", "Client+Server support for SeeAllItems overlay.", "autogen")]
+[assembly: ModInfoAttribute("See All Items", "Shows all items in the creative inventory.", "someone")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
