64b40db8f5933b0a8af6c21903a22be7d53d1b47
vos6434 <kacper11122003@wp.pl>
2026-02-25 22:20:23 +0000

    new mod loads

--- Nostalgia/AssemblyInfo.cs
+++ Nostalgia/AssemblyInfo.cs
@@ -1,6 +1,3 @@
-using System.Reflection;
 using BetaSharp.Modding;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: ModInfo("Nostalgia", "ModAuthor", "A small nostalgia-themed mod")] 
+[assembly: ModInfo("Nostalgia", "ModAuthor", "A small nostalgia-themed mod")]
--- Nostalgia/NostalgiaBase.cs
+++ Nostalgia/NostalgiaBase.cs
@@ -8,6 +8,21 @@ public class NostalgiaBase : ModBase
 {
     public override bool HasOptionsMenu => false;
 
+    public NostalgiaBase()
+    {
+        Name = "Nostalgia";
+        Description = "A small nostalgia-themed mod";
+        Author = "ModAuthor";
+        Version = "1.0.0";
+        Logger = Microsoft.Extensions.Logging.Abstractions.NullLogger.Instance;
+    }
+
+    // Backwards-compatible constructor used by the mod loader when it expects a parameterized ctor.
+    public NostalgiaBase(string name, string author, string description, string version, Microsoft.Extensions.Logging.ILogger logger)
+        : base(name, author, description, version, logger)
+    {
+    }
+
     public override void Initialize(Side side)
     {
         Console.WriteLine("Initialize called for Nostalgia mod");
